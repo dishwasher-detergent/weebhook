80815567cee5ddaf2f89b4989eea6b6bdf1fb917
Kenneth Bass <kenny_bass@outlook.com>
2024-10-06 17:33:26 -0500

    rem,ove shadow

--- components/ui/request.tsx
+++ components/ui/request.tsx
@@ -3,8 +3,6 @@
 import { Badge } from "@/components/ui/badge";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
-import { LucideChevronDown } from "lucide-react";
-import { useState } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
 
@@ -17,64 +15,50 @@ export interface RequestProps {
 
 export function Request({ timestamp, method, headers, body }: RequestProps) {
   const headerObj: Record<string, string> = JSON.parse(headers);
-  const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <div className="p-2 rounded-xl border border-dashed bg-muted/50 space-y-4 shadow-sm">
-      <header
-        className="flex flex-row gap-2 items-center justify-between cursor-pointer"
-        onClick={() => setOpen(!open)}
-      >
+    <div className="p-2 rounded-xl border border-dashed bg-muted/50 space-y-4">
+      <header className="flex flex-row gap-2 items-center justify-between cursor-pointer">
         <div className="flex flex-row gap-2 items-center">
           <Badge variant={method}>{method.toUpperCase()}</Badge>
           <p className="text-muted-foreground text-sm font-semibold">
             {timestamp}
           </p>
         </div>
-        <LucideChevronDown className={`size-4 ${!open ? "rotate-180" : ""}`} />
       </header>
-      {open && (
-        <>
-          <div>
-            <h4 className="font-semibold text-primary text-base mb-1 ml-2">
-              Headers
-            </h4>
-            <div className="rounded-xl border border-dashed overflow-x-auto bg-background">
-              {Object.keys(headerObj).length > 0 && (
-                <Table>
-                  <TableBody>
-                    {Object.entries(headerObj).map(([key, value]) => (
-                      <TableRow key={key}>
-                        <TableCell>{key}</TableCell>
-                        <TableCell>{value}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              )}
-              {Object.keys(headerObj).length === 0 && (
-                <div className="p-4 text-muted-foreground font-bold text-sm">
-                  No header values found.
-                </div>
-              )}
+
+      <div>
+        <h4 className="font-semibold text-primary text-base mb-1 ml-2">
+          Headers
+        </h4>
+        <div className="rounded-xl border border-dashed overflow-x-auto bg-background">
+          {Object.keys(headerObj).length > 0 && (
+            <Table>
+              <TableBody>
+                {Object.entries(headerObj).map(([key, value]) => (
+                  <TableRow key={key}>
+                    <TableCell>{key}</TableCell>
+                    <TableCell>{value}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          )}
+          {Object.keys(headerObj).length === 0 && (
+            <div className="p-4 text-muted-foreground font-bold text-sm">
+              No header values found.
             </div>
-          </div>
-          <div>
-            <h4 className="font-semibold text-primary text-base mb-1 ml-2">
-              Body
-            </h4>
-            <div className="p-4 rounded-xl border border-dashed bg-background code">
-              <SyntaxHighlighter
-                language="js"
-                style={coy}
-                showLineNumbers={true}
-              >
-                {JSON.stringify(JSON.parse(body), null, 2)}
-              </SyntaxHighlighter>
-            </div>
-          </div>
-        </>
-      )}
+          )}
+        </div>
+      </div>
+      <div>
+        <h4 className="font-semibold text-primary text-base mb-1 ml-2">Body</h4>
+        <div className="p-4 rounded-xl border border-dashed bg-background code">
+          <SyntaxHighlighter language="js" style={coy} showLineNumbers={true}>
+            {JSON.stringify(JSON.parse(body), null, 2)}
+          </SyntaxHighlighter>
+        </div>
+      </div>
     </div>
   );
 }
