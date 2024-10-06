d85c60bdab28315266cfe8c5b9f54616349000ce
Kenneth Bass <kenny_bass@outlook.com>
2024-10-06 17:52:47 -0500

    redirect to latest project if none are set

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,57 @@
 "use client";
 
 import { projectId } from "@/atoms/project";
+import { Button } from "@/components/ui/button";
+import { createClient, getLoggedInUser } from "@/lib/client/appwrite";
+import { DATABASE_ID, PROJECT_COLLECTION_ID } from "@/lib/constants";
 
-import { useAtomValue } from "jotai";
+import { Models } from "appwrite";
+import { useAtom } from "jotai";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { Query } from "node-appwrite";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const projectIdValue = useAtomValue(projectId);
+  const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
+    null
+  );
+  const [projectIdValue, setProjectId] = useAtom(projectId);
   const router = useRouter();
+  const { database } = createClient();
 
   useEffect(() => {
+    async function getProjects() {
+      const data = await database.listDocuments(
+        DATABASE_ID,
+        PROJECT_COLLECTION_ID,
+        [Query.orderDesc("$createdAt"), Query.limit(1)]
+      );
+
+      if (data.documents.length > 0) {
+        setProjectId(data.documents[0].$id);
+      }
+    }
+
     if (projectIdValue) {
       router.replace(projectIdValue);
+    } else {
+      getProjects();
     }
   }, [projectIdValue]);
+
+  useEffect(() => {
+    async function getUser() {
+      const user = await getLoggedInUser();
+
+      setUser(user);
+    }
+
+    getUser();
+  }, []);
+
+  return (
+    <main>
+      {user ? <Button>Create Webhook</Button> : <Button>Sign Up</Button>}
+    </main>
+  );
 }
