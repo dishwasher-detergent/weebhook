8c54e07376ae7720e73e205cc385a376b7677c09
Kenneth Bass <kenny_bass@outlook.com>
2024-10-08 19:14:23 -0500

    better landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,21 @@
 
 import { projectId } from "@/atoms/project";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { createClient, getLoggedInUser } from "@/lib/client/appwrite";
 import { DATABASE_ID, PROJECT_COLLECTION_ID } from "@/lib/constants";
 import { createWebhook } from "@/lib/utils";
 
 import { Models } from "appwrite";
 import { useAtom } from "jotai";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Query } from "node-appwrite";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [projectIdValue, setProjectId] = useAtom(projectId);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [user, setUser] = useState<Models.User<Models.Preferences> | null>(
     null
   );
@@ -21,33 +24,33 @@ export default function Home() {
   const { database } = createClient();
 
   useEffect(() => {
-    async function checkLoginStatus() {
+    async function checkLoginStatusAndProjects() {
+      setIsLoading(true);
+
       const userData = await getLoggedInUser();
       setUser(userData);
+
+      if (userData) {
+        const data = await database.listDocuments(
+          DATABASE_ID,
+          PROJECT_COLLECTION_ID,
+          [Query.orderDesc("$createdAt"), Query.limit(1)]
+        );
+
+        if (data.documents.length > 0) {
+          setProjectId(data.documents[0].$id);
+        }
+      }
+
+      setIsLoading(false);
     }
 
-    checkLoginStatus();
+    checkLoginStatusAndProjects();
   }, []);
 
   useEffect(() => {
-    async function getProjects() {
-      const data = await database.listDocuments(
-        DATABASE_ID,
-        PROJECT_COLLECTION_ID,
-        [Query.orderDesc("$createdAt"), Query.limit(1)]
-      );
-
-      if (data.documents.length > 0) {
-        setProjectId(data.documents[0].$id);
-      }
-    }
-
-    if (user) {
-      if (projectIdValue) {
-        router.replace(projectIdValue);
-      } else {
-        getProjects();
-      }
+    if (user && projectIdValue) {
+      router.replace(projectIdValue);
     }
   }, [user, projectIdValue]);
 
@@ -60,13 +63,28 @@ export default function Home() {
     }
   }
 
-  if (!user) {
-    return <div>Please log in to view your projects.</div>;
-  }
-
   return (
-    <div>
-      <Button onClick={create}>Create Webhook</Button>
-    </div>
+    <>
+      <header className="w-full border-b sticky top-0 bg-background/50 backdrop-blur-sm z-10">
+        <div className="max-w-4xl mx-auto p-4 px-8">
+          <p className="font-bold text-primary text-sm">Webhook</p>
+          {isLoading ? (
+            <Skeleton className="h-9 w-36" />
+          ) : user ? (
+            <Button onClick={create}>Create Webhook</Button>
+          ) : (
+            <Button asChild>
+              <Link href="/login">Login</Link>
+            </Button>
+          )}
+        </div>
+      </header>
+      <main className="max-w-4xl mx-auto space-y-4 p-4 px-8">
+        <h2 className="font-bold text-primary">Requests Per Hour</h2>
+        <Skeleton className="w-full h-48" />
+        <h2 className="font-bold text-primary">Requests</h2>
+        <Skeleton className="w-full h-48" />
+      </main>
+    </>
   );
 }
