95383ba70584298583a3c0645cfd0c8444181b83
Kenneth Bass <kenny_bass@outlook.com>
2024-10-24 10:14:50 -0500

    fix padding

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,7 +3,7 @@ import { Project } from "@/components/project";
 export function Nav() {
   return (
     <header className="w-full border-b sticky top-0 bg-background/50 backdrop-blur-sm z-10">
-      <div className="max-w-4xl mx-auto px-4 md:px-8">
+      <div className="max-w-4xl mx-auto p-4 md:px-8">
         <Project />
       </div>
     </header>
