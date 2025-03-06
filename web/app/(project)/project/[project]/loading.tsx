a38ac83b808bcc0b4d13f39b3e6a82984a56efe4
Kenneth Bass <kenny_bass@outlook.com>
2025-03-05 20:22:25 -0600

    update loading

--- web/app/(project)/project/[project]/loading.tsx
+++ web/app/(project)/project/[project]/loading.tsx
@@ -1,10 +1,18 @@
 import { RequestsChartLoading } from "@/components/request-chart-loading";
 import { RequestLoading } from "@/components/request-loading";
+import { Badge } from "@/components/ui/badge";
 
 export default async function Loading() {
   return (
     <>
       <main className="mx-auto max-w-4xl space-y-4 p-4 px-4 md:px-8">
+        <Badge
+          variant="outline"
+          className="inline-flex flex-row gap-2 px-2 py-1"
+        >
+          Fetching Requests
+          <div className="size-2 animate-pulse rounded-full bg-amber-500 ring-4 ring-amber-500/25" />
+        </Badge>
         <RequestsChartLoading />
         <RequestLoading />
       </main>
--- web/components/request-chart-loading.tsx
+++ web/components/request-chart-loading.tsx
@@ -19,10 +19,10 @@ export function RequestsChartLoading() {
             Showing total requests by the hour
           </CardDescription>
         </div>
-        <div className="relative z-30 flex flex-none flex-col justify-center gap-1 border-t border-dashed bg-muted/50 px-6 py-2 text-left even:border-l sm:border-l sm:border-t-0">
-          <span className="text-xs text-muted-foreground">Requests</span>
-          <span className="text-lg font-bold leading-none sm:text-3xl">
-            <Skeleton className="h-12 w-full" />
+        <div className="bg-muted/50 relative z-30 flex flex-none flex-col justify-center gap-1 border-t border-dashed px-6 py-2 text-left even:border-l sm:border-t-0 sm:border-l">
+          <span className="text-muted-foreground text-xs">Requests</span>
+          <span className="text-lg leading-none font-bold sm:text-3xl">
+            <Skeleton className="h-9 w-full" />
           </span>
         </div>
       </CardHeader>
